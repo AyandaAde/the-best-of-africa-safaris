c51b2d1940ddcaddda33a6f6d5536c7aaf3ab15c
Ayanda Ade <ayandakinyambo27@gmail.com>
2024-05-29 18:45:51 +0200

    dark mode update

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,6 @@ export default function RootLayout({
       <body className={`${poppins.className} ${chillax.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
